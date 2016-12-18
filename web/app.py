60846a70022063ec176d3878e8127ee99836a0f3
Eric Dahl <eric.c.dahl@gmail.com>
2016-12-18 00:47:02 -0500

    cache headers

--- web/app.py
+++ web/app.py
@@ -1,15 +1,18 @@
-from flask import Flask
+#from flask import Flask
+import flask
 import os
 
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 @app.route('/')
 def hello_world():
     #r = 'Flask Dockerized. PORT is ' + os.environ['PORT'] + '\n'
     r = str(os.environ)
     r = 'Hello from ' + os.environ['HOSTNAME'] + '\n'
+    resp = flask.Response(r)
+    resp.headers['Cache-Control'] = 'public, max-age=5'
+    return resp
     #return 'Flask Dockerized. PORT is ' + os.environ['PORT'] + '\n'
-    return r
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
