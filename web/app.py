9b84622e9f6ee843c6ea4319c224502a5cbf3aa6
Eric Dahl <eric.c.dahl@gmail.com>
2016-12-18 17:26:35 -0500

    add redis

--- web/app.py
+++ web/app.py
@@ -1,14 +1,18 @@
 #from flask import Flask
 import flask
 import os
-
+import redis
 
 app = flask.Flask(__name__)
 @app.route('/')
 def hello_world():
-    #r = 'Flask Dockerized. PORT is ' + os.environ['PORT'] + '\n'
+
+    redis_handle = redis.StrictRedis(host='redis', port=6379, db=0)
+    redis_handle.set('foo', 'bar')
+    c = redis_handle.incr('counter')
+
     r = str(os.environ)
-    r = 'Hello from ' + os.environ['HOSTNAME'] + '\n'
+    r = 'Hello from ' + os.environ['HOSTNAME'] + ' (count is: ' + str(c) + ')\n'
     resp = flask.Response(r)
     resp.headers['Cache-Control'] = 'public, max-age=5'
     return resp
