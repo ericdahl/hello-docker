6a959a20241c44e4ee3861dacdb3479c4694c171
Eric Dahl <eric.c.dahl@gmail.com>
2016-12-18 18:01:01 -0500

    web volumes

--- web/app.py
+++ web/app.py
@@ -1,4 +1,3 @@
-#from flask import Flask
 import flask
 import os
 import redis
@@ -8,15 +7,13 @@ app = flask.Flask(__name__)
 def hello_world():
 
     redis_handle = redis.StrictRedis(host='redis', port=6379, db=0)
-    redis_handle.set('foo', 'bar')
     c = redis_handle.incr('counter')
 
     r = str(os.environ)
     r = 'Hello from ' + os.environ['HOSTNAME'] + ' (count is: ' + str(c) + ')\n'
-    resp = flask.Response(r)
+    resp = flask.make_response(r)
     resp.headers['Cache-Control'] = 'public, max-age=5'
     return resp
-    #return 'Flask Dockerized. PORT is ' + os.environ['PORT'] + '\n'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
