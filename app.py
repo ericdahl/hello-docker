f3ce9891044095bb77fee34c74014cd42dd6c090
Eric Dahl <eric.c.dahl@gmail.com>
2016-12-18 00:24:08 -0500

    working nginx with 3 backends

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def hello_world():
     return 'Flask Dockerized. PORT is ' + os.environ['PORT'] + '\n'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
